2d247398db2a2bf39fccd06971f085ba89a1f450
screw-pack <gaillard.germain@gmail.com>
2020-10-29 15:23:37 +0100

    Back to something simple, KISS.

--- test_flask.py
+++ test_flask.py
@@ -10,19 +10,13 @@ app = Flask(__name__)
 app.debug = True
 mazer = Mazer()
 
-
-@app.route('/mazer', methods = ['POST', 'GET'])
+@app.route('/mazer')
 def maze_gen():
-    if request.method == 'POST':
-        width = request.form.get('width')
-        height = request.form.get('height')
-        maze = mazer.gen(int(width), int(height))
-        maze.set_square(10)
-        return render_template('gen.html', square_x = maze.square_x,
-                                square_y = maze.square_y, maze_width = maze.width,
-                                maze_height = maze.height, maze = maze.maze)
-    else:
-        return render_template('index.html', square_x = 3, square_y = 3)
+    width = request.args.get('width', '31')
+    height = request.args.get('height', '31')
+    maze = mazer.gen(int(width), int(height))
+    maze.set_square(10)
+    return render_template('index.html', maze = maze)
 
 
 if __name__ == '__main__':
