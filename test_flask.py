003d843fdc5c82efee337df9fe6ba5df1ebd1a1e
screw-pack <gaillard.germain@gmail.com>
2020-10-29 11:50:59 +0100

    Changed to POST method

--- test_flask.py
+++ test_flask.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from mazer import Mazer
 
 
@@ -11,16 +11,18 @@ app.debug = True
 mazer = Mazer()
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        width = request.args.get('width', '31')
-        height = request.args.get('height', '31')
+@app.route('/mazer', methods = ['POST', 'GET'])
+def maze_gen():
+    if request.method == 'POST':
+        width = request.form.get('width')
+        height = request.form.get('height')
         maze = mazer.gen(int(width), int(height))
         maze.set_square(10)
-    return render_template('index.html', square_x = maze.square_x,
-                           square_y = maze.square_y, maze_width = maze.width,
-                           maze_height = maze.height, maze = maze.maze)
+        return render_template('gen.html', square_x = maze.square_x,
+                                square_y = maze.square_y, maze_width = maze.width,
+                                maze_height = maze.height, maze = maze.maze)
+    else:
+        return render_template('index.html', square_x = 3, square_y = 3)
 
 
 if __name__ == '__main__':
